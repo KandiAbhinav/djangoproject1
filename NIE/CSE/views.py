64111e4c872ebbbf3cd6f6a9881551b847cd29f4
KandiAbhinav <151983305+KandiAbhinav@users.noreply.github.com>
2023-11-25 09:44:25 +0530

    Add files via upload

---
+++ NIE/CSE/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def product(request):
+    return render(request, 'product.html')
+def contact(request):
+    return render(request, 'contact.html')
+def service(request):
+    return render(request, 'service.html')
+def aboutus(request):
+    return render(request, 'aboutus.html')
